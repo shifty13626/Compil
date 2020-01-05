aa51196c69e8b82bed14ce83e93f47833e54f34a
Ludo <shifty13626@gmail.com>
2020-01-05 21:52:28 +0100

    dossier tests + rapport

--- Compil/Compil/Tests/test.c
+++ Compil/Compil/Tests/test.c
@@ -1,39 +1,19 @@
 #include<stdlib>
 #include<stdio>
 
-#define LIMIT 15
-
-function test(a, b)
-{
-    var i;
-    for (i = 0; i < 5; i = i + 1) {
-        print(i);
-        var j;
-            for (j = 0; j < 3; j = j + 1) {
-                print(j);
-                break;
-            }
-    }
-
-
-    return a + b;
-}
-
 function main()
 {
-<<<<<<< HEAD
 	var i;
-	do
+	var j;
+
+	for (i = 0; i < 5; i = i + 1)
 	{
-		i = i + 1;
 		print(i);
-	} while (i < 5);
+		for (j = 10; j > 7; j = j - 1)
+		{
+			print(j);
+		}
+	}
 
 	return 0;
-=======
-    var i = 2 ^ 3;
-    i = test(i, 2);
-    print(i);
-    return 0;
->>>>>>> declaration_opti
 }
